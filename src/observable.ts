d27a38a56387ba9171dca15f0a6277f11847f593
amosbaby <931963081@qq.com>
2022-05-23 16:14:50 +0800

    add function observer

--- src/observable.ts
+++ src/observable.ts
@@ -13,14 +13,25 @@ export class AMObservable<T> {
   }
 
   /**
-   * 接收一个观察者
+   * 接收一个观察者对象
    * @param observer 它有三个方法：
    * 1. next
    * 2. error
    * 3. complete
    */
   subscribe(observer: any) {
-    this._subscribe(observer)
+    const defaultObserver = {
+      next: (value: any) => { },
+      error: (err: any) => { },
+      complete: () => { },
+    }
+    if (typeof observer === 'function') {
+      // 如果传入的是一个方法，则将next替换成observer
+      this._subscribe({ ...defaultObserver, next: observer })
+    } else {
+
+      this._subscribe({ ...defaultObserver, ...observer })
+    }
   }
 }
 
--- src/of/of-test.ts
+++ src/of/of-test.ts
@@ -1,4 +1,4 @@
-import { of } from 'rxjs'
+// import { of } from 'rxjs'
 import { of as amOf } from './of'
 
 export const observer = {
@@ -8,10 +8,16 @@ export const observer = {
 }
 
 
-const dataStream$ = of(1, 2, 3)
+// const dataStream$ = of(1, 2, 3)
 
-dataStream$.subscribe(observer)
+// dataStream$.subscribe(observer)
 
-const dataStream2$ = amOf(5, 6, 7)
+// 测试手写实现的of
+// const dataStream2$ = amOf(5, 6, 7)
 
-dataStream2$.subscribe(observer)
+// dataStream2$.subscribe(observer)
+
+// 测试subscribe传入一个函数
+const dataStream3$ = amOf(7, 8, 7)
+
+dataStream3$.subscribe(console.log)
